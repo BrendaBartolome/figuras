6b73595a8a2d0c534d07cc4b7b7fdfff28b99704
BrendaBartolome <43483462+BrendaBartolome@users.noreply.github.com>
2018-10-24 17:15:51 -0500

    Add files via upload

---
+++ CUADRADO.java
@@ -0,0 +1,15 @@
+package Figuras;
+
+/**
+ *
+ * @author itzel
+ */
+public class Rectagulo {
+    double altura, base, area;
+    Rectagulo(double a, double b, double c)
+    {
+        this.altura=a;
+        this.base=b;
+        this.area=c;
+        
+    }
